e2e3635183ece8c290117f6f5645bc627fb92aa7
jacopomucci <mucci.jacopo@gmail.com>
2020-05-27 21:48:25 +0200

    confronto datasets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ChartOptions } from "chart.js";
+import { Subject } from "rxjs";
 
 @Component({
   selector: "app-root",
@@ -17,7 +18,8 @@ export class AppComponent {
     fieldY: null,
   };
   fieldY: string;
-  chartData = {};
+
+  updateCharts$ = new Subject<void>();
 
   getProperty(obj) {
     return Object.getOwnPropertyNames(obj);
@@ -26,33 +28,32 @@ export class AppComponent {
     this.inputData = dataObj;
     this.fields = this.getProperty(dataObj[0]);
   }
-  setChartData() {
-    const chartData = {
-      labels: this.inputData.map((object) => {
-        return object[this.selectedFields.fieldX];
-      }),
-      datasets: [
-        {
-          label: this.selectedFields.fieldY,
-          data: this.inputData.map((object) => {
-            return object[this.selectedFields.fieldY];
-          }),
-          backgroundColor: "#3252a8",
-          hoverBackgroundColor: "#4d6cbf",
-          borderColor: "#3252a8",
-          pointBackgroundColor: "#3252a8",
-          borderWidth: 3,
-          fill: true,
-        },
-      ],
-    };
-    this.chartData = chartData;
-    console.log(chartData);
-  }
+  // setChartData() {
+  //   const chartData = {
+  //     labels: this.inputData.map((object) => {
+  //       return object[this.selectedFields.fieldX];
+  //     }),
+  //     datasets: [
+  //       {
+  //         label: this.selectedFields.fieldY,
+  //         data: this.inputData.map((object) => {
+  //           return object[this.selectedFields.fieldY];
+  //         }),
+  //         backgroundColor: "#3252a8",
+  //         hoverBackgroundColor: "#4d6cbf",
+  //         borderColor: "#3252a8",
+  //         pointBackgroundColor: "#3252a8",
+  //         borderWidth: 3,
+  //         fill: true,
+  //       },
+  //     ],
+  //   };
+  //   this.chartData = chartData;
+  //   console.log(chartData);
+  // }
   setFields(fields: [string, string]) {
-    console.log(fields);
     this.selectedFields.fieldX = fields[0];
     this.selectedFields.fieldY = fields[1];
-    setTimeout(() => this.setChartData(), 500);
+    this.updateCharts$.next();
   }
 }
--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -5,23 +5,25 @@ import {
   SimpleChanges,
   ViewChild,
   Input,
+  OnDestroy,
 } from "@angular/core";
 
 import { BaseChartDirective, Label } from "ng2-charts";
-import { ChartOptions } from "chart.js";
+import { ChartOptions, ChartDataSets } from "chart.js";
+import { AppComponent } from "src/app/app.component";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-charts",
   templateUrl: "./charts.component.html",
   styleUrls: ["./charts.component.css"],
 })
-export class ChartsComponent implements OnInit, OnChanges {
-  @Input() chartData;
+export class ChartsComponent implements OnInit, OnDestroy {
+  @Input() data: any[] = [];
+  @Input() fields: { fieldX: string; fieldY: string };
   @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
 
-  datasets = [];
-  labels = [];
-  colors = [];
+  sub: Subscription;
 
   chartOptions: ChartOptions = {
     legend: {
@@ -29,16 +31,54 @@ export class ChartsComponent implements OnInit, OnChanges {
     },
   };
 
-  constructor() {}
+  chartLabels: Label[] = [];
+
+  chartData: ChartDataSets[] = [
+    {
+      label: "Field X",
+      data: [],
+      backgroundColor: "#3252a8",
+      hoverBackgroundColor: "#4d6cbf",
+      hoverBorderColor: "#3252a8",
+      borderColor: "#3252a8",
+      pointBackgroundColor: "#3252a8",
+      borderWidth: 3,
+      fill: true,
+    },
+    {
+      label: "Field Y",
+      data: [],
+      backgroundColor: "#34c9eb",
+      hoverBackgroundColor: "#34c9eb",
+      hoverBorderColor: "#34c9eb",
+      borderColor: "#34c9eb",
+      pointBackgroundColor: "#34c9eb",
+      borderWidth: 3,
+      fill: true,
+    },
+  ];
+
+  constructor(private app: AppComponent) {}
 
   ngOnInit() {
-    this.datasets = [{ data: [], label: "" }];
-    this.labels = [];
+    this.sub = this.app.updateCharts$.subscribe(() => {
+      this.chartLabels = this.data.map((object) => {
+        return object[this.fields.fieldX];
+      });
+      this.chartData[0].data = this.data.map((object) => {
+        return object[this.fields.fieldX];
+      });
+      this.chartData[0].label = this.fields.fieldX;
+      this.chartData[1].data = this.data.map((object) => {
+        return object[this.fields.fieldY];
+      });
+      this.chartData[1].label = this.fields.fieldY;
+
+      this.chart.update();
+    });
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.datasets = this.chartData.datasets;
-    this.labels = this.chartData.labels;
-    this.chart.update();
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
